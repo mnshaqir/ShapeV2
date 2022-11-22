47707261b4fc5f1d268c8f0cf2f765c6dbd9cde6
mnshaqir <116859685+mnshaqir@users.noreply.github.com>
2022-11-22 14:56:38 +0800

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Rectangle
+    {
+         // property (Encapsulation)
+            private double _width;
+            private double _height;
+
+            public double Width
+            {
+                get { return _width; }
+                set { _width = value; }
+            }
+
+            public double Height
+            {
+                get { return _height; }
+                set { _height = value; }
+            }
+        //constructor
+            public Rectangle()
+            {
+                _width = 0;
+                _height = 0;
+            }
+        //constructor
+            public Rectangle(double width, double height)
+            {
+                _width = width;
+                _height = height;
+            }
+            
+        //method
+            public double Area()
+            {
+                return _width * _height;
+
+            }
+        //method
+            public double Circumference()
+            {
+                return 2 * (_width + _height);
+
+            }
+        }
+
+    
+}
---
+++ Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Square: Rectangle
+    {
+        private double _len;
+
+        public double Len
+        {
+            get { return _len; }
+            set { _len = value; }
+        }
+
+        public Square()
+        {
+            _len = 0;
+        }
+
+        public Square(double len)
+        {
+            _len = len;
+        }
+        
+        public new double Area()
+        {
+            return _len * _len;
+        }
+
+        public new double Circumference()
+        {
+            return 4 * _len;
+        }
+    }
+}
